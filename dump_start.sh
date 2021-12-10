3f1838693303089e36d108a4320a80065879ffee
houssemriden <cloud_user@ec75a065371c.mylabserver.com>
2021-12-10 19:35:08 +0000

    ansible everywhere

--- dump_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-echo " tcpdump started in forescout ..."
-
-
-ssh forescout@172.31.105.106  tcpdump -i replay -w capture.pcap  2> /dev/null & 
--- stop_tcp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-  echo " Stopping tcpreplay "
-  p=$(ssh forescout@172.31.105.106 ps -few | grep foresc | grep tcpreplay |awk 'NR==1 {print $2}')
-
-   ssh forescout@172.31.105.106  kill -15 $p
